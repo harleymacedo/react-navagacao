994622d70658296643afa998fbabe349d9410f58
harleymacedo <harley.ip@gmail.com>
2022-12-20 18:34:08 -0300

    React Router funcionando normalmente

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Empresa from './pages/Empresa';
 import Contato from './pages/Contato';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/empresa'>Empresa</Link></li>
-                    <li><Link to='/contato'>Contato</Link></li>
-                </ul>
-            </nav>
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/empresa' element={<Empresa />} />
                     <Route path='/contato' element={<Contato />} />
                 </Routes>            
             </BrowserRouter>
-        </div>
     );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href='/'>Home</a></li>
+                    <li><a href='/contato'>Contato</a></li>
+                    <li><a href='/empresa'>Empresa</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
